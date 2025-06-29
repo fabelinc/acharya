648a71eeb3861cb4fd723ea24096f1394e2ff685
Alok Rana <alokrana@Aloks-Mac-mini.local>
2025-06-28 17:57:10 -0700

    Update UI

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import './App.css';
+import './App.less';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') // No "as HTMLElement"
