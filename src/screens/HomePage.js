845351fb70826c77c509da92d4e221e7524e8e07
Alok Rana <alokrana@Aloks-Mac-mini.local>
2025-07-20 13:55:10 -0700

    update caraousel_deepti

--- src/screens/HomePage.js
+++ src/screens/HomePage.js
@@ -110,22 +110,22 @@ const LandingPage = () => {
         >
         <Carousel autoplay effect="fade" dots className="carousel-fixed-height">
           {heroSlides.map((slide, index) => (
-            <div key={index} className="relative w-full h-full">
-              <div className="w-full h-full overflow-hidden">
-                <img
-                  src={slide.image}
-                  alt={slide.caption}
-                  className="carousel-image"
-                />
-              </div>
-              <div className="absolute bottom-4 left-1/2 transform -translate-x-1/2 w-full px-4 max-w-xs sm:max-w-md text-center">
-                <div className="bg-black bg-opacity-60 px-4 py-2 rounded-md shadow-md">
-                  <Title level={4} className="text-white text-sm sm:text-base md:text-lg font-medium m-0">
-                    {slide.caption}
-                  </Title>
-                </div>
+            <div className="w-full h-[300px] sm:h-[400px] md:h-[500px] lg:h-[550px] relative">
+            <img
+              src={slide.image}
+              alt={slide.caption}
+              className="w-full h-full object-cover"
+              style={{ zIndex: 1 }}
+            />
+            <div className="absolute bottom-4 left-1/2 transform -translate-x-1/2 w-full px-4 max-w-xs sm:max-w-md text-center z-10">
+              <div className="bg-black bg-opacity-60 px-4 py-2 rounded-md shadow-md">
+                <Title level={4} className="text-white text-sm sm:text-base md:text-lg font-medium m-0">
+                  {slide.caption}
+                </Title>
               </div>
             </div>
+          </div>
+          
           ))}
         </Carousel>
         </motion.div>
